a822b0f29764c4e54773c8fb70736d643a466b22
Mohaddeseh_Ganjkhanlou <160696821+Mohaddeseh02@users.noreply.github.com>
2024-05-10 19:23:22 +0330

    In the second part, I completed the run function

--- src/main/java/sbu/cs/MatrixMultiplication.java
+++ src/main/java/sbu/cs/MatrixMultiplication.java
@@ -8,11 +8,34 @@ public class MatrixMultiplication {
     // You are allowed to change all code in the BlockMultiplier class
     public static class BlockMultiplier implements Runnable
     {
-
+        private final int[][] A;
+        private final int[][] B;
+        private final int[][] C;
+        private final int startRow;
+        private final int endRow;
+        List<List<Integer>> tempMatrixProduct;
+        public BlockMultiplier(int[][] A, int[][] B, int[][] C, int startRow, int endRow) {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+            this.startRow = startRow;
+            this.endRow = endRow;
+        }
 
         @Override
         public void run() {
-
+            for (int i = startRow; i < endRow; i++) {
+                List<Integer> row = new ArrayList<>();
+                for (int j = 0; j < B[0].length; j++) {
+                    int sum = 0;
+                    for (int k = 0; k < A[0].length; k++) {
+                        sum += A[i][k] * B[k][j];
+                    }
+                    row.add(sum);
+                    C[i][j] = sum;
+                }
+                tempMatrixProduct.add(row);
+            }
         }
     }
 
@@ -22,7 +45,7 @@ public class MatrixMultiplication {
     both p and r are even numbers
     */
     public static List<List<Integer>> ParallelizeMatMul(List<List<Integer>> matrix_A, List<List<Integer>> matrix_B){
-
+        
     }
 
     public static void main(String[] args) {
